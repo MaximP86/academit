6ea6d564111e7f45b6e89e4eda5dedf831b740f8
MaximP86 <maximpp386@yandex.ru>
2016-07-06 20:57:37 +0600

    Temperature Converter

--- TemperatureConverter/src/Converter.java
+++ TemperatureConverter/src/Converter.java
@@ -6,33 +6,27 @@ public class Converter {
     private double fahrenheit;
     private double kelvin;
 
-    public Converter(double celsius, double fahrenheit, double kelvin) {
-        this.celsius = celsius;
-        this.fahrenheit = fahrenheit;
-        this.kelvin = kelvin;
-    }
-
-    public double getCelsiusToFahrenheit() {
+    public double getCelsiusToFahrenheit(double celsius) {
         return (celsius * 1.8) + 32;
     }
 
-    public double getCelsiusToKelvin() {
+    public double getCelsiusToKelvin(double celsius) {
         return celsius + 273.15;
     }
 
-    public double getFahrenheitToCelsius() {
+    public double getFahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) / 1.8;
     }
 
-    public double getFahrenheitToKelvin() {
+    public double getFahrenheitToKelvin(double fahrenheit) {
         return (fahrenheit - 32) / 1.8 + 273.15;
     }
 
-    public double getKelvinToCelsius() {
+    public double getKelvinToCelsius(double kelvin) {
         return kelvin - 273.15;
     }
 
-    public double getKelvinToFahrenheit() {
+    public double getKelvinToFahrenheit(double kelvin) {
         return ((kelvin - 273.15) * 1.8) + 32;
     }
 
--- TemperatureConverter/src/Main.java
+++ TemperatureConverter/src/Main.java
@@ -1,13 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by 1 on 06.07.2016.
  */
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Temperature Converter");
-        frame.setSize(300, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("Temperature Converter");
+                frame.setSize(300, 200);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+                JButton button = new JButton("Convert");
+                frame.add(button);
+            }
+        });
     }
 }
